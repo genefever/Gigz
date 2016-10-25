c135fafb6efc84250fd60e4642c9071300869c68
Edward Huang <egunawan@uscd.edu>
2016-10-24 17:22:15 -0700

    add ejs template to the web

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var express = require('express');
-
 var app = express();
 
 
+app.use(express.static('public/css')); // get the css file in public
+app.set("view engine", "ejs");
+
+
 app.get('/', (req, res)=>{
-  res.send('Hello Mo better!!');
+  // res.send('Hello Mo better!!');
+  res.render("frontPage");
 });
 
+app.get('/login', (req, res) => {
+  res.send('Login');
+});
+
+app.get('/secondPage', (req,res)=>{
+  res.send('secondPage');
+});
 
 app.listen(3000, ()=>{
   console.log("Server listen in localhost 3000.....");
